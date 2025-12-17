a6013d384989c9e26bb9b8d43039245aad0a5791
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-12-17 12:01:57 +0300

    Update №12.3 Изменение списка.py

--- №12.3 Изменение списка.py
+++ №12.3 Изменение списка.py
@@ -1,13 +1,1 @@
-marks = [5, 4, 3, 5, 2, 5, 4, 3, 5, 5]
-
-count_fives = 0
-count_twos = 0
-
-for i in marks:
-    if i == 5:
-        count_fives += 1
-    elif i == 2:
-        count_twos += 1
-
-print(f'Количество пятерок: {count_fives}')
-print(f'Количество двоек: {count_twos}')
+awsd
