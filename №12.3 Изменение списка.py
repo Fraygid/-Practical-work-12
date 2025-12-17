4c2f1bfead01938216390915a636a94d136059ea
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-12-17 12:05:08 +0300

    Update №12.3 Изменение списка.py

--- №12.3 Изменение списка.py
+++ №12.3 Изменение списка.py
@@ -1,1 +1,9 @@
-awsd
+users = ['Admin', 'Guest', 'User', 'Bot']
+
+print(f'Изначальный вид списка: {users}')
+
+users[2] = 'Moderator'
+users[-1] = 'SuperAdmin'
+users.append('NewBie')
+
+print(f'Итоговый список: {users}')
